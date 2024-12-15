0c7317492c9148094dce7a512b669f47fd60aff8
Virrann <cunha.vargas.oliveira@gmail.com>
2024-12-15 17:56:12 -0300

    first

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+#define PATHU "/home/$USER/.local/share/applications/"
+#define PATHA "/usr/share/applications/"
+
+int main(int argc, char const *argv[]){
+
+
+    /* code */
+    return 0;
+}
